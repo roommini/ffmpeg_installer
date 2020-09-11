072c2d1443e4614792f9f8fd540785e9c120d713
Chanchai Srithep <roommini@hotmail.com>
2020-09-12 02:58:47 +0700

    Update ff_installer.sh
    update all

--- ff_installer.sh
+++ ff_installer.sh
@@ -18,11 +18,22 @@ yum -y install yum-utils
 yum -y install freetype-devel
 
 # Install the dependencies
-yum-config-manager --add-repo http://www.nasm.us/nasm.repo
 yum -y install autoconf automake bzip2 cmake gcc gcc-c++ git libtool make mercurial nasm pkgconfig zlib-devel
 
 # Make a directory for FFmpeg sources
 mkdir ~/ffmpeg_sources
+
+# Install NASM
+cd ~/ffmpeg_sources
+curl -O -L https://www.nasm.us/pub/nasm/releasebuilds/2.14.02/nasm-2.14.02.tar.bz2
+tar xjvf nasm-2.14.02.tar.bz2
+cd nasm-2.14.02
+./autogen.sh
+./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
+make
+make install
+make distclean
+source ~/.bash_profile
 
 # Install Yasm
 cd ~/ffmpeg_sources
@@ -59,9 +70,9 @@ source ~/.bash_profile
 
 # Install libmp3lame
 cd ~/ffmpeg_sources
-curl -L -O http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz
-tar xzvf lame-3.99.5.tar.gz
-cd lame-3.99.5
+curl -O -L https://downloads.sourceforge.net/project/lame/lame/3.100/lame-3.100.tar.gz
+tar xzvf lame-3.100.tar.gz
+cd lame-3.100
 ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --disable-shared --enable-nasm
 make
 make install
@@ -70,9 +81,9 @@ source ~/.bash_profile
 
 # Install libopus
 cd ~/ffmpeg_sources
-curl -O https://archive.mozilla.org/pub/opus/opus-1.1.5.tar.gz
-tar xzvf opus-1.1.5.tar.gz
-cd opus-1.1.5
+curl -O -L https://archive.mozilla.org/pub/opus/opus-1.3.1.tar.gz
+tar xzvf opus-1.3.1.tar.gz
+cd opus-1.3.1
 ./configure --prefix="$HOME/ffmpeg_build" --disable-shared
 make
 make install
@@ -80,27 +91,27 @@ make distclean
 source ~/.bash_profile
 
 # Install libogg
-cd ~/ffmpeg_sources
-curl -O http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz
-tar xzvf libogg-1.3.2.tar.gz
-cd libogg-1.3.2
-./configure --prefix="$HOME/ffmpeg_build" --disable-shared
-make
-make install
-make distclean
-source ~/.bash_profile
+#cd ~/ffmpeg_sources
+#curl -O http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz
+#tar xzvf libogg-1.3.2.tar.gz
+#cd libogg-1.3.2
+#./configure --prefix="$HOME/ffmpeg_build" --disable-shared
+#make
+#make install
+#make distclean
+#source ~/.bash_profile
 #yum -y install libogg
 
 # Install libvorbis
-cd ~/ffmpeg_sources
-curl -O http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.4.tar.gz
-tar xzvf libvorbis-1.3.4.tar.gz
-cd libvorbis-1.3.4
-./configure --prefix="$HOME/ffmpeg_build" --with-ogg="$HOME/ffmpeg_build" --disable-shared
-make
-make install
-make distclean
-source ~/.bash_profile
+#cd ~/ffmpeg_sources
+#curl -O http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.4.tar.gz
+#tar xzvf libvorbis-1.3.4.tar.gz
+#cd libvorbis-1.3.4
+#./configure --prefix="$HOME/ffmpeg_build" --with-ogg="$HOME/ffmpeg_build" --disable-shared
+#make
+#make install
+#make distclean
+#source ~/.bash_profile
 #yum -y install libvorbis
 
 # Install libtheora
@@ -113,7 +124,7 @@ source ~/.bash_profile
 # make install
 # make distclean
 # source ~/.bash_profile
-yum -y install libtheora
+#yum -y install libtheora
 
 # Install libvpx
 cd ~/ffmpeg_sources
